9dc73a9afcf5684a00c3cee83b6674f7636ba973
KuboniZA <sipumzo.lucwaba@gmail.com>
2026-04-21 19:40:24 +0200

    Render movies list on frontend. Add a keyExtractor, and text styling.

--- services/useFetch.ts
+++ services/useFetch.ts
@@ -1,56 +1,39 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
   const [data, setData] = useState<T | null>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
 
-  const isMounted = useRef<boolean>(true);
-
-  useEffect(() => {
-    return () => {
-      isMounted.current = false;
-    };
-  });
-
-  const fetchData = useCallback(async () => {
-    setLoading(true);
-    setError(null);
-
+  const fetchData = async () => {
     try {
-      const result = await fetchFunction();
-
-      if (isMounted.current) {
-        setData(result);
-      }
-    } catch (err) {
-      if (isMounted.current) {
-        setError(
-          err instanceof Error
-            ? err
-            : new Error("An error occurred while fetching data."),
-        );
-      }
-    } finally {
-      if (isMounted.current) {
-        setLoading(false);
-      }
-    }
-  }, [fetchFunction]);
-
-  const reset = useCallback(() => {
-    if (isMounted.current) {
-      setData(null);
-      setLoading(false);
+      setLoading(true);
       setError(null);
-    }
-  }, []);
 
+      const result = await fetchFunction();
+      setData(result);
+    } catch (err) {
+      // @ts-ignore
+      setError(
+        err instanceof Error
+          ? err
+          : new Error("An error occurred while fetching data."),
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const reset = () => {
+    setData(null);
+    setLoading(false);
+    setError(null);
+  };
   useEffect(() => {
     if (autoFetch) {
       fetchData();
     }
-  }, [autoFetch, fetchData]);
+  }, []);
 
   return { data, loading, error, refetch: fetchData, reset };
 };
