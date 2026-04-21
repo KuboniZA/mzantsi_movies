557e3419d9c8e5e4ae50a040f35effa84a8e0ffa
KuboniZA <sipumzo.lucwaba@gmail.com>
2026-04-21 23:05:25 +0200

    Add debounce to searching and error screens for false searches.

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -25,15 +25,14 @@ const Search = () => {
   );
 
   useEffect(() => {
-    const func = async () => {
+    const timeoutId = setTimeout(async () => {
       if (searchQuery.trim()) {
         await loadMovies();
       } else {
         reset();
       }
-    };
-
-    func();
+    }, 500);
+    return () => clearTimeout(timeoutId);
   }, [searchQuery]);
 
   return (
@@ -89,6 +88,15 @@ const Search = () => {
                 </Text>
               )}
           </>
+        }
+        ListEmptyComponent={
+          !moviesLoading && !moviesError ? (
+            <View className="mt-10 px-5">
+              <Text className="text-center text-gray-500">
+                {searchQuery.trim() ? "No movies found" : "Search for a movie"}
+              </Text>
+            </View>
+          ) : null
         }
       />
     </View>
