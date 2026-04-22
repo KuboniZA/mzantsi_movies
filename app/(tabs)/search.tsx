edaa7f6688a5307fc2d75888bd68e468b4220214
KuboniZA <sipumzo.lucwaba@gmail.com>
2026-04-22 19:22:30 +0200

    Add and configure appwrite database.

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -3,6 +3,7 @@ import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { fetchMovies } from "@/services/api";
+import { updateSearchCount } from "@/services/appwrite";
 import useFetch from "@/services/useFetch";
 import { useEffect, useState } from "react";
 import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
@@ -24,16 +25,26 @@ const Search = () => {
     false,
   );
 
+  // Search and debounce useEffect
+
   useEffect(() => {
     const timeoutId = setTimeout(async () => {
       if (searchQuery.trim()) {
         await loadMovies();
+        if (movies?.length > 0 && movies?.[0])
+          await updateSearchCount(searchQuery, movies[0]);
       } else {
         reset();
       }
     }, 500);
     return () => clearTimeout(timeoutId);
   }, [searchQuery]);
+
+  // useEffect(() => {
+  //   if (searchQuery.trim() && movies?.length > 0) {
+  //     updateSearchCount(searchQuery, movies[0]);
+  //   }
+  // }, [movies, searchQuery]);
 
   return (
     <View className="flex-1 bg-primary">
